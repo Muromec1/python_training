8f83e64d668691f65fe9fe5028cfe0ecde17cf3c
T360-v2-DTPB-s06 <ilja9753@gmail.com>
2022-03-02 15:32:51 +0100

    Kifejezések témakör

---
+++ user_interface.py
@@ -0,0 +1,10 @@
+# name = input("Add meg a neved!")
+# print(name)
+
+# number = input("Adj meg egy szamot, és én megszorzom ottel")
+# print(type(number))
+# print(int(number) * 5)
+#
+# print(int("abc"))
+
+
