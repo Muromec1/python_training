08457697c2efffce55afba522e570f00b74dd654
T360-v2-DTPB-s06 <ilja9753@gmail.com>
2022-03-02 13:56:20 +0100

    git ignore

--- hello.py
+++ hello.py
@@ -6,3 +6,8 @@ print("Hello World") # Kiírja, h hello world
 print("Hello World")
 print("Hello World")
 print("Hello GitHub")
+
+print(""""Aprócska kalapocska
+benne csacska
+macska
+mocska""")
---
+++ statements.py
@@ -0,0 +1,19 @@
+print(5 * 6)
+print(5 + 6)
+print(3 / 2)
+print(3 - 2)
+print(5 * 6 + 12)
+print((1 + 2) * 3)
+print("alma" + "körte")
+
+# Mi van ha össze akarsz adni egy stringet és egy int-et?
+# Mi van fordítva
+# Mi van ha egy stringből ki akarsz vonni egy másikat
+# Mi van ha egy stringet megszorzol egy másik stringgel?
+# Mi van, ha egy stringet megszorzol egy int-tel?
+
+print("tojás" * 5)
+print("tojás" + 5)
+print(5 + "tojás")
+print("tojás" - "fehérje")
+print("tojás" * "fehérje")
