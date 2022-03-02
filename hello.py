55e9d659018fe1c41d43b41c6a78f46be68fd730
Résztvevő - 6 <T360-v2-DTPB-s06@oktatas.local>
2022-03-02 11:55:48 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+# Ez az első programom
+
+
+
+print("Hello World") # Kiírja, h hello world
+print("Hello World")
+print("Hello World")
---
+++ variables.py
@@ -0,0 +1,39 @@
+print(5)
+print(100_000)
+
+print(3.14)
+
+employee_name = "John Doe"
+print(employee_name)
+
+age = 30
+print(age)
+age = 31
+print(age)
+
+color_of_eye = "Blue"
+print(color_of_eye)
+
+color_of_eye_of_John = color_of_eye
+print(color_of_eye)
+print(color_of_eye_of_John)
+
+print(type(5)) # int = integer
+print(type(3.14)) # float = lebegőpontos szám
+print(type("John Doe")) # str = string - karakterlánc
+
+age = 32
+print(type(age))
+
+age = "harminckettő"
+print(type(age))
+print(age)
+print(type(age))
+
+# type hint
+
+salary: int = 200
+print(salary)
+salary = "kétszáz"
+print(salary)
+
