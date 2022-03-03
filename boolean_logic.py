6ba94791e66892a3fb65a31a188e1a86dbab635e
T360-v2-DTPB-s06 <ilja9753@gmail.com>
2022-03-03 10:17:29 +0100

    Boolean elmélet

---
+++ boolean_logic.py
@@ -0,0 +1,17 @@
+print(not True)
+print(not False)
+i = 5
+print(i < 10)
+
+print(True and True)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True)
+
+i = 5
+print(i > 3 and i < 10) # == print(3 < i < 10)
+
+fruit = "apple"
+print(fruit == "raspberry" or fruit == "apple")
