c314ab7f80cbcd3ba9652cacfa7f85dfb99ce843
T360-v2-DTPB-s06 <ilja9753@gmail.com>
2022-03-03 09:23:52 +0100

    Nap eleji gyakorlás

---
+++ Gyakorlás.py
@@ -0,0 +1,26 @@
+# Van egy lista, teli számokkal. Írd ki az összegüket
+# sum = 0
+# for i in [1, 2, 4, 67, 122, 345]:
+#     sum = sum+i
+# print()
+
+# Tanár
+# sum = 0
+# for number in [5, 7, 2, 1]:
+#     sum = sum + number
+# print(sum)
+
+# Tanár2
+# sum = 0
+# numbers = [5, 7, 2, 1]
+# for number in numbers:
+#     sum = sum + number
+# print(sum)
+
+# Tanár3
+sum = 0
+numbers = [5, 7, 2, 1]
+for number in numbers:
+    # sum = sum + number
+    sum += number
+print(sum)
