e50bc240954d67b233941549356e4c5ab1119955
T360-v2-DTPB-s06 <ilja9753@gmail.com>
2022-03-03 10:08:25 +0100

    Elágazás + gyakorlás

---
+++ input_data.py
@@ -0,0 +1,20 @@
+# Kérjük be a nevét, ha üres, akkor írjuk ki, hogy nem lehet üres értéket megadni,
+# és kérjük be mégegyszer
+# ha nem üres, írjuk ki, hogy köszönöm
+
+# Enyém
+# name = input("Hogyan hívnak? ")
+# while name == "":
+#     print("Nem lehet üres értéket megadni!")
+#     name = input("Légyszi írd be még1szer!")
+# print("Köszi!")
+
+
+# Tanár
+# name = ""
+# while name == "":
+#     name = input("Add meg a nevedet! ")
+#     if name == "":
+#         print("Nem lehet üres értéket megadni!")
+# print("Üdvözöllek " + name)
+
---
+++ selection.py
@@ -0,0 +1,18 @@
+# name = input("Adj meg a neved!")
+# if len(name) == 0:
+#     print("Nem jó, ne adj meg üres nevet")
+
+
+# if name == "":
+#     print("Nem jó, ne adj meg üres nevet")
+
+
+# if :
+#     print("Hello World")
+# print("end")
+
+# Órai feladat
+# age = int(input("Add meg az életkorodat! "))
+# if age < 20:
+#     print("Túl fiatal vagy!")
+# print("End")
